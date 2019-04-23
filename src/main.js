78511a103cbd0d7948ad43e56e9b1591f9fdf123
Mariia <maria.bookvoed@gmail.com>
2019-04-23 19:26:13 -0400

    cards

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ let card = '';
 //imprimir tarjetas
 window.addEventListener('load', function() {
     for (let i = 0; i < listaPokemones.length; i++) {
-        card += `<div class="card"><img alt="" class="card-img-top planta-veneno" src=${listaPokemones[i].img}><div class = "card-body"> <h5 class = "card-title">${listaPokemones[i].name}</h5><p class="card-text">Nº${listaPokemones[i].num}</p><a class = "btn btn-primary btn-tarjeta" data-target="#exampleModalCenter" data-toggle="modal" href="#">ver mas</a></div></div>`;
+        card += `<div class="card" id="${listaPokemones[i].id}"><img alt="" class="card-img-top planta-veneno" src=${listaPokemones[i].img}><div class = "card-body"> <h5 class = "card-title">${listaPokemones[i].name}</h5><p class="card-text">Nº${listaPokemones[i].num}</p><a class = "btn btn-primary btn-tarjeta" data-target="#myModal" data-toggle="modal" href="#">ver mas</a></div></div>`;
     }
     document.getElementById('tarjetas').innerHTML = card;
 });
