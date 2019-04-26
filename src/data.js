23a66b062516f44558b1eba8bde9be1074d0666b
Mariia <maria.bookvoed@gmail.com>
2019-04-26 13:01:09 -0400

    Filtramos

--- src/data.js
+++ src/data.js
@@ -1,10 +1,11 @@
 /* Manejo de data */
-
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
-const example = () => {
-  return 'example';
-};
-
-window.example = example;
+const filterType = (arr, condition) => {
+    const arrType = arr.filter(el => {
+        return el.type.includes(condition);
+    });
+    //console.log(arrType);
+    return arrType;
+}
+window.filterType = filterType;
